0561da67f7d846f357dcdc4bd8a910b7e86e9104
SuJiaKuan <feabries@gmail.com>
2021-07-03 16:43:49 +0800

    Update server.js for basic news feed flow

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
+const { collectTodayNews } = require('./news');
+const { sendMessage } = require('./slack');
+const config = require('./config');
+
 function main() {
+  setTimeout(async () => {
+    try {
+      const newsInfos = await collectTodayNews(
+        config.news.keywords,
+        process.env.NEWS_API_KEY,
+      );
+      for (newsInfo of newsInfos) {
+        const message = `${newsInfo.title}\n${newsInfo.url}`;
+
+        await sendMessage(
+          message,
+          config.slack.channel,
+          process.env.SLACK_TOKEN,
+        );
+      }
+      console.log('Success for news feed')
+    } catch (err) {
+      console.error(err);
+    }
+  }, 0);
 }
 
 main();
